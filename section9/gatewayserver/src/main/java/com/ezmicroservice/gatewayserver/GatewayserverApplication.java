eb89114ae66b2b3e5cdaaa63f69118659bc34896
trangbn580 <trangbn580@gmail.com>
2024-07-04 17:29:14 +0700

    Add response header for customer Java-based routes

--- section9/gatewayserver/src/main/java/com/ezmicroservice/gatewayserver/GatewayserverApplication.java
+++ section9/gatewayserver/src/main/java/com/ezmicroservice/gatewayserver/GatewayserverApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class GatewayserverApplication {
 
@@ -18,15 +20,18 @@ public class GatewayserverApplication {
 		return builder.routes()
 				.route(p -> p
 						.path("/ezmicroservice/accounts/**")
-						.filters(f->f.rewritePath("/ezmicroservice/accounts/(?<segment>.*)", "/${segment}"))
+						.filters(f->f.rewritePath("/ezmicroservice/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://ACCOUNTS"))
 				.route(p -> p
 						.path("/ezmicroservice/loans/**")
-						.filters(f -> f.rewritePath("/exmicroservice/loans/(?<segment>.*)", "/${segment}"))
+						.filters(f -> f.rewritePath("/exmicroservice/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://LOANS"))
 				.route(p -> p
 						.path("/ezmicroservice/cards/**")
-						.filters(f -> f.rewritePath("/ezmicroservice/cards/(?<segment>.*)", "/${segment}"))
+						.filters(f -> f.rewritePath("/ezmicroservice/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://CARDS"))
 				.build();
 	}
