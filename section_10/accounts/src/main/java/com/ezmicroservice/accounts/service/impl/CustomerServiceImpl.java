8f170e6f282a4d9325bc56bcf22b444d73ea047c
trangbn580 <trangbn580@gmail.com>
2024-07-10 22:45:50 +0700

    Add circuitbreaker for Accounts

--- section_10/accounts/src/main/java/com/ezmicroservice/accounts/service/impl/CustomerServiceImpl.java
+++ section_10/accounts/src/main/java/com/ezmicroservice/accounts/service/impl/CustomerServiceImpl.java
@@ -47,10 +47,15 @@ public class CustomerServiceImpl implements ICustomerService {
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
 
         ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
-        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        if (null != loansDtoResponseEntity) {
+            customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        }
+
 
         ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCards(correlationId, mobileNumber);
-        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        if (null != cardsDtoResponseEntity) {
+            customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        }
 
         return customerDetailsDto;
     }
