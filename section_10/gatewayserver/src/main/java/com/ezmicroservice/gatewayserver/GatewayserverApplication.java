90b58353341652c45a0e79bed6e5107f78c82903
trangbn580 <trangbn580@gmail.com>
2024-07-19 07:28:13 +0700

    Add retries pattern for accounts services part 2

--- section_10/gatewayserver/src/main/java/com/ezmicroservice/gatewayserver/GatewayserverApplication.java
+++ section_10/gatewayserver/src/main/java/com/ezmicroservice/gatewayserver/GatewayserverApplication.java
@@ -52,12 +52,12 @@ public class GatewayserverApplication {
 				.build();
 	}
 
-//	@Bean
-//	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer(){
-//		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-//				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-//				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build()
-//		);
-//	}
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build()
+		);
+	}
 
 }
